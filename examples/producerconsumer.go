bbda1ccd0d71ddc2f756e9c85a95781498d29b3a
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-08-07 14:47:09 -0700

    Added Examples

---
+++ examples/producerconsumer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sandeepkv93/thread-safe-queue/queue"
+)
+
+func producer(q *queue.ThreadSafeQueue) {
+	for i := 0; i < 10; i++ {
+		q.Enqueue(i)
+	}
+}
+
+func consumer(q *queue.ThreadSafeQueue) {
+	for i := 0; i < 10; i++ {
+		item, _ := q.Dequeue()
+		fmt.Println("Consumed:", item)
+	}
+}
+
+func main() {
+	q := queue.NewThreadSafeQueue()
+
+	go producer(q)
+	go consumer(q)
+
+	time.Sleep(time.Second) // Allow time for producer and consumer to complete
+}
